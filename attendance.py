6391325cccd61afe262a16bc8e43e868212a0c63
chandrika-kp <102351627+chandrika-kp@users.noreply.github.com>
2022-03-25 13:44:04 +0530

    Create attendance.py

---
+++ attendance.py
@@ -0,0 +1,22 @@
+
+No_of_students = int(input("Enter number of students : "))
+No_of_days = int(input("Enter number of days : "))
+Students = []
+for i in range(No_of_students):
+    i = input("Enter student name:"+str(i+1)+"   ")
+    Students.append(i)
+Students.sort()
+print(Students)
+empty_list = [0] * (len(Students))
+for j in range(No_of_days):
+    for k in range(No_of_students):
+        l = input("If %s was present today: Enter p if NOT Enter a :     " % Students[k])
+        if l == 'p':
+            ndx = Students.index(Students[k])
+            empty_list[ndx] += 1
+Total_attendance = empty_list
+for i in range(len(Students)):
+    Roll_no = i+1
+    Day_attendance = Total_attendance[i]
+    Attendance_percentage = (Day_attendance/No_of_days)*100
+    print(Roll_no, Students[i].ljust(10, ' '), Attendance_percentage)
